41d7d82b9d3077a1c165c7fa5bba574089c698a3
David Carl <david.carl@hotmail.ch>
2022-08-16 09:03:25 +0200

    Added documentation for coef method.

--- R/coef-method.R
+++ R/coef-method.R
@@ -1,4 +1,13 @@
-#' @exportS3Method
+#' Extract Model Coefficients of TSCI Fits
+#'
+#' @param object a object of class 'tsci'.
+#' @param all logical. If \code{FALSE}, only the treatment effect estimate of the
+#' selected violation space candidate(s) is returned. If \code{TRUE}, additionally the treatment
+#' effect estimates of each violation space candidate are returned.
+#' @param ... arguments to be passed to or from other methods.
+#'
+#' @return Coefficients extracted form the model object \code{object}.
+#' @export
 coef.tsci <- function(object,
                       all = FALSE,
                       ...) {
